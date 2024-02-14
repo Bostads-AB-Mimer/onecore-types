bae50e264210b2fcd153da24c9ce481216165b99
Fredrik Hasselgren <fredrik.hasselgren@prototyp.se>
2024-02-14 19:19:38 +0100

    feat: 1161 updates types when reading contract from xpand db (#17)
    * feat: 1161 updates types when reading contract from xpand db
    
    * feat: 1161 remove redudant lastUpdated fields
    
    * feat: 1161 update lease ids and leases on contact

--- types.ts
+++ types.ts
@@ -1,35 +1,48 @@
 import { LeaseStatus } from './enums';
 
 interface Contact {
-  contactId: string;
-  leaseId: string | undefined;
-  lease: Lease | undefined;
-  firstName: string;
-  lastName: string;
-  fullName: string;
-  type: string;
-  nationalRegistrationNumber: string;
-  birthDate: Date;
-  address: Address | undefined;
-  mobilePhone: string;
-  phoneNumber: string;
-  emailAddress: string;
-  lastUpdated: Date | undefined;
+    contactCode: string //cmctc.cmctckod
+    contactKey: string //cmtct.keycmctc
+    leaseIds?: string[]
+    leases?: Lease[]
+    firstName: string
+    lastName: string
+    fullName: string
+    nationalRegistrationNumber: string
+    birthDate: Date
+    address: Address | undefined
+    phoneNumbers: PhoneNumber[] | undefined
+    emailAddress: string,
+    isTenant: boolean,
+}
+
+interface PhoneNumber {
+    phoneNumber: string
+    type: string
+    isMainNumber: boolean
 }
 
 interface Lease {
-  leaseId: string;
-  leaseNumber: string;
-  leaseStartDate: Date;
-  leaseEndDate: Date | undefined;
-  status: LeaseStatus;
-  tenantContactIds: string[] | undefined;
-  tenants: Contact[] | undefined;
-  rentalPropertyId: string;
-  rentalProperty: RentalProperty | undefined;
-  type: string;
-  rentInfo: RentInfo | undefined;
-  lastUpdated: Date | undefined;
+    leaseId: string
+    leaseNumber: string
+    leaseStartDate: Date
+    leaseEndDate: Date | undefined
+    status: LeaseStatus
+    tenantContactIds: string[] | undefined
+    tenants: Contact[] | undefined
+    rentalPropertyId: string
+    rentalProperty: RentalProperty | undefined
+    type: string
+    rentInfo: RentInfo | undefined
+    address: Address | undefined
+    noticeGivenBy: string | undefined,
+    noticeDate: Date | undefined,
+    noticeTimeTenant: string | undefined,
+    preferredMoveOutDate: Date | undefined,
+    terminationDate: Date | undefined,
+    contractDate: Date | undefined,
+    lastDebitDate: Date | undefined,
+    approvalDate: Date | undefined,
 }
 
 interface RentalProperty {
