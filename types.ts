1e1845f761dcb6a9c5746d63dff170ca5042c1f3
Anders Norrback Bornholm <anders@bornholm.se>
2024-02-20 09:59:05 +0100

    Added stuff

--- types.ts
+++ types.ts
@@ -1,143 +1,157 @@
-import { LeaseStatus } from "./enums";
+import {
+  LeaseStatus,
+  ParkingSpaceApplicationCategory,
+  ParkingSpaceType,
+} from './enums'
 
 interface Contact {
-  contactCode: string; //cmctc.cmctckod
-  contactKey: string; //cmtct.keycmctc
-  leaseIds?: string[];
-  leases?: Lease[];
-  firstName: string;
-  lastName: string;
-  fullName: string;
-  nationalRegistrationNumber: string;
-  birthDate: Date;
-  address: Address | undefined;
-  phoneNumbers: PhoneNumber[] | undefined;
-  emailAddress: string;
-  isTenant: boolean;
+  contactCode: string //cmctc.cmctckod
+  contactKey: string //cmtct.keycmctc
+  leaseIds?: string[]
+  leases?: Lease[]
+  firstName: string
+  lastName: string
+  fullName: string
+  nationalRegistrationNumber: string
+  birthDate: Date
+  address: Address | undefined
+  phoneNumbers: PhoneNumber[] | undefined
+  emailAddress: string
+  isTenant: boolean
 }
 
 interface PhoneNumber {
-  phoneNumber: string;
-  type: string;
-  isMainNumber: boolean;
+  phoneNumber: string
+  type: string
+  isMainNumber: boolean
 }
 
 interface Lease {
-  leaseId: string;
-  leaseNumber: string;
-  leaseStartDate: Date;
-  leaseEndDate: Date | undefined;
-  status: LeaseStatus;
-  tenantContactIds: string[] | undefined;
-  tenants: Contact[] | undefined;
-  rentalPropertyId: string;
-  rentalProperty: RentalProperty | undefined;
-  type: string;
-  rentInfo: RentInfo | undefined;
-  address: Address | undefined;
-  noticeGivenBy: string | undefined;
-  noticeDate: Date | undefined;
-  noticeTimeTenant: string | undefined;
-  preferredMoveOutDate: Date | undefined;
-  terminationDate: Date | undefined;
-  contractDate: Date | undefined;
-  lastDebitDate: Date | undefined;
-  approvalDate: Date | undefined;
+  leaseId: string
+  leaseNumber: string
+  leaseStartDate: Date
+  leaseEndDate: Date | undefined
+  status: LeaseStatus
+  tenantContactIds: string[] | undefined
+  tenants: Contact[] | undefined
+  rentalPropertyId: string
+  rentalProperty: RentalProperty | undefined
+  type: string
+  rentInfo: RentInfo | undefined
+  address: Address | undefined
+  noticeGivenBy: string | undefined
+  noticeDate: Date | undefined
+  noticeTimeTenant: string | undefined
+  preferredMoveOutDate: Date | undefined
+  terminationDate: Date | undefined
+  contractDate: Date | undefined
+  lastDebitDate: Date | undefined
+  approvalDate: Date | undefined
 }
 
 interface RentalProperty {
-  rentalPropertyId: string;
-  apartmentNumber: number;
-  size: number;
-  type: string;
-  address: Address | undefined;
-  rentalPropertyType: string;
-  additionsIncludedInRent: string;
-  otherInfo: string | undefined;
-  roomTypes: Array<RoomType> | undefined;
-  lastUpdated: Date | undefined;
+  rentalPropertyId: string
+  apartmentNumber: number
+  size: number
+  type: string
+  address: Address | undefined
+  rentalPropertyType: string
+  additionsIncludedInRent: string
+  otherInfo: string | undefined
+  roomTypes: Array<RoomType> | undefined
+  lastUpdated: Date | undefined
 }
 
 interface Address {
-  street: string;
-  number: string;
-  postalCode: string;
-  city: string;
+  street: string
+  number: string
+  postalCode: string
+  city: string
 }
 
 interface RentInfo {
-  currentRent: Rent;
-  futureRents: Array<Rent> | undefined;
+  currentRent: Rent
+  futureRents: Array<Rent> | undefined
 }
 
 interface Rent {
-  rentId: string;
-  leaseId: string;
-  currentRent: number;
-  additionalChargeDescription: string | undefined;
-  additionalChargeAmount: number | undefined;
-  rentStartDate: Date;
-  rentEndDate: Date | undefined;
+  rentId?: string
+  leaseId?: string
+  currentRent: number
+  vat: number
+  additionalChargeDescription: string | undefined
+  additionalChargeAmount: number | undefined
+  rentStartDate: Date | undefined
+  rentEndDate: Date | undefined
 }
 
 interface RoomType {
-  roomTypeId: string;
-  name: string;
-  materialOptionGroups?: Array<MaterialOptionGroup>;
+  roomTypeId: string
+  name: string
+  materialOptionGroups?: Array<MaterialOptionGroup>
 }
 
 interface MaterialChoice {
-  materialChoiceId: string;
-  materialOptionId: string;
-  materialOptionGroupId: string;
-  apartmentId: string;
-  roomTypeId: string;
-  materialOption?: MaterialOption | undefined;
-  materialOptionGroup?: MaterialOptionGroup | undefined;
-  roomType?: RoomType | undefined;
-  status: string; //TODO enum of Draft/Submitted/Cancelled
-  dateOfSubmission?: Date;
-  dateOfCancellation?: Date;
+  materialChoiceId: string
+  materialOptionId: string
+  materialOptionGroupId: string
+  apartmentId: string
+  roomTypeId: string
+  materialOption?: MaterialOption | undefined
+  materialOptionGroup?: MaterialOptionGroup | undefined
+  roomType?: RoomType | undefined
+  status: string //TODO enum of Draft/Submitted/Cancelled
+  dateOfSubmission?: Date
+  dateOfCancellation?: Date
 }
 
 interface MaterialOptionGroup {
-  materialOptionGroupId: string;
-  roomTypeId: string;
-  roomTypeName?: string;
-  name?: string;
-  actionName?: string;
-  materialOptions?: Array<MaterialOption>;
-  materialChoices?: Array<MaterialChoice>;
-  type: string; //TODO enum of Concept/AddOn/SingleChoice
+  materialOptionGroupId: string
+  roomTypeId: string
+  roomTypeName?: string
+  name?: string
+  actionName?: string
+  materialOptions?: Array<MaterialOption>
+  materialChoices?: Array<MaterialChoice>
+  type: string //TODO enum of Concept/AddOn/SingleChoice
 }
 
 interface MaterialOption {
-  materialOptionId: string;
-  caption: string;
-  shortDescription?: string;
-  description?: string;
-  coverImage?: string;
-  images?: Array<string>;
-  roomTypeName?: string;
-  materialOptionGroupName?: string;
+  materialOptionId: string
+  caption: string
+  shortDescription?: string
+  description?: string
+  coverImage?: string
+  images?: Array<string>
+  roomTypeName?: string
+  materialOptionGroupName?: string
 }
 
 interface ConsumerReport {
-  pnr: string;
-  template: string;
-  status: string;
-  status_text: string;
-  errorList: Array<ConsumerReportError> | [];
-  name: string;
-  address: string;
-  zip: string;
-  city: string;
+  pnr: string
+  template: string
+  status: string
+  status_text: string
+  errorList: Array<ConsumerReportError> | []
+  name: string
+  address: string
+  zip: string
+  city: string
 }
 
 interface ConsumerReportError {
-  Cause_of_Reject: string;
-  Reject_text: string;
-  Reject_comment: string;
+  Cause_of_Reject: string
+  Reject_text: string
+  Reject_comment: string
+}
+
+interface ParkingSpace {
+  parkingSpaceId: string
+  address: Address
+  rent: RentInfo
+  vacantFrom: Date
+  type: ParkingSpaceType
+  applicationCategory: ParkingSpaceApplicationCategory
 }
 
 export {
@@ -153,4 +167,5 @@ export {
   MaterialChoice,
   ConsumerReport,
   ConsumerReportError,
-};
+  ParkingSpace,
+}
