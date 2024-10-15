31139ac5a1e95501b0c900fa82f716c9c5b5d927
driatic <fredrik.hasselgren@prototyp.se>
2024-10-15 15:23:14 +0200

    feat: add more values to CreateNoteOfInterestErrorCodes

--- src/error-types/create-note-of-interest-error-codes.ts
+++ src/error-types/create-note-of-interest-error-codes.ts
@@ -1,5 +1,11 @@
 enum CreateNoteOfInterestErrorCodes {
+  ParkingspaceNotFound = 'parkingspace-not-found',
+  ParkingspaceNotInternal = 'parkingspace-not-internal',
+  ApplicantNotFound = 'applicant-not-found',
+  ApplicantNotTenant = 'applicant-not-tenant',
+  NotEligibleToRent = 'not-eligible-to-rent',
   InternalCreditCheckFailed = 'internal-credit-check-failed',
+  InternalError = 'internal-error',
   Unknown = 'unknown'
 }
 
