e413d9c36d9e8ad54c731c73ce00d733ac1c726e
Andreas Lundqvist <31646645+momentiris@users.noreply.github.com>
2024-10-28 15:10:01 +0100

    MIM-169: Add ListingStatus NoApplicants and Closed (#111)
    * add closed and no applicants listing status
    
    * add err code
    
    * add error codes for update listing status

--- src/enums.ts
+++ src/enums.ts
@@ -51,8 +51,9 @@ enum PaymentStatus {
 enum ListingStatus {
   Active = 1, //Publicerad
   Assigned = 2,
-  Deleted = 3,
+  Closed = 3,
   Expired = 4,
+  NoApplicants = 5,
 }
 
 enum ApplicantStatus {
