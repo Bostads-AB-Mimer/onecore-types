ecc0939a6bcd0742e8d42bef32da60a5d32dcbf5
momentiris <momentiris@gmail.com>
2024-11-06 15:13:46 +0100

    update application profile

--- src/leasing/application-profile.ts
+++ src/leasing/application-profile.ts
@@ -1,3 +1,13 @@
 import { ApplicationProfileSchema } from '../schemas'
 
 export const GetApplicationProfileResponseDataSchema = ApplicationProfileSchema
+
+export const UpdateApplicationProfileRequestParamsSchema =
+  ApplicationProfileSchema.pick({
+    numAdults: true,
+    numChildren: true,
+    expiresAt: true,
+  })
+
+export const UpdateApplicationProfileResponseDataSchema =
+  ApplicationProfileSchema
