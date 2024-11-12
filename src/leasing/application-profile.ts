20364cfc323218bea4c4ead264c0b41403806959
Andreas Lundqvist <31646645+momentiris@users.noreply.github.com>
2024-11-12 11:18:44 +0100

    feat: UTH-24 - update application profile (#124)
    * update application profile
    
    * update -> createOrUpdate

--- src/leasing/application-profile.ts
+++ src/leasing/application-profile.ts
@@ -1,3 +1,13 @@
 import { ApplicationProfileSchema } from '../schemas'
 
 export const GetApplicationProfileResponseDataSchema = ApplicationProfileSchema
+
+export const CreateOrUpdateApplicationProfileRequestParamsSchema =
+  ApplicationProfileSchema.pick({
+    numAdults: true,
+    numChildren: true,
+    expiresAt: true,
+  })
+
+export const CreateOrUpdateApplicationProfileResponseDataSchema =
+  ApplicationProfileSchema
