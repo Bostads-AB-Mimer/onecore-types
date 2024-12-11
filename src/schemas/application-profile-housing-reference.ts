4593bca754fa8a356556160432132c582c6dfa68
Andreas Lundqvist <momentiris@gmail.com>
2024-12-11 15:05:37 +0100

    add todo

--- src/schemas/application-profile-housing-reference.ts
+++ src/schemas/application-profile-housing-reference.ts
@@ -11,8 +11,8 @@ export const HousingReferenceReviewStatusSchema = z.enum([
 export const ApplicationProfileHousingReferenceSchema = z.object({
   id: z.number(),
   applicationProfileId: z.number(),
-  phone: z.string(),
-  email: z.string().nullable(),
+  phone: z.string(), // TODO: Should parse as phone number
+  email: z.string().nullable(), // TODO: Should parse as email
   reviewStatus: HousingReferenceReviewStatusSchema,
   reviewStatusReason: z.string().nullable(),
   reviewedAt: z.coerce.date().nullable(),
