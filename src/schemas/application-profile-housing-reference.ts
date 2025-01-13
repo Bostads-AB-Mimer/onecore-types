a1aad5a4e508f106ee89b1152c9e36e0960040ce
Andreas Lundqvist <31646645+momentiris@users.noreply.github.com>
2025-01-13 13:39:07 +0100

    feat: UTH-164 - Add types for housingType and reviewStatus (#136)
    * add types for housingType and reviewStatus
    
    * add todo
    
    * adapt application profile housing reference schema to db schema changes
    
    * require all properties for now

--- src/schemas/application-profile-housing-reference.ts
+++ src/schemas/application-profile-housing-reference.ts
@@ -1,13 +1,32 @@
 import { z } from 'zod'
 
+export const HousingReferenceReviewStatusSchema = z.enum([
+  'APPROVED',
+  'REJECTED',
+  'CONTACTED_UNREACHABLE',
+  'PENDING',
+  'REFERENCE_NOT_REQUIRED',
+])
+
+export const HousingReferenceReasonRejectedSchema = z.enum([
+  'DISTURBANCE',
+  'LATE_RENT_PAYMENT',
+  'DEBT_TO_LANDLORD',
+  'MISMANAGEMENT',
+])
+
 export const ApplicationProfileHousingReferenceSchema = z.object({
   id: z.number(),
   applicationProfileId: z.number(),
-  phone: z.string(),
-  email: z.string().nullable(),
-  reviewStatus: z.string(),
-  reviewStatusReason: z.string().nullable(),
-  reviewedAt: z.coerce.date().nullable(),
+  phone: z.string().nullable(), // TODO: Should parse as phone number
+  email: z.string().nullable(), // TODO: Should parse as email
+  reviewStatus: HousingReferenceReviewStatusSchema,
+  comment: z.string().nullable(),
+  reasonRejected: HousingReferenceReasonRejectedSchema.nullable(),
+  lastAdminUpdatedAt: z.coerce.date().nullable(),
+  lastAdminUpdatedBy: z.string().nullable(),
+  lastApplicantUpdatedAt: z.coerce.date().nullable(),
+
   expiresAt: z.coerce.date(),
   createdAt: z.coerce.date(),
 })
