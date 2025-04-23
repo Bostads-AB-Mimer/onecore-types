b460a90676f9e86289ffd9140321f8c68a54a977
henrikhenrikpersson <henrik.persson@iteam.se>
2025-04-23 14:15:38 +0200

    feat: MIM-492: visa arenden for lagenhet i property tree (#170)
    * Make WorkOrderRow properties nullable
    
    * Add Url to WorkOrder
    
    ---------
    
    Co-authored-by: Henrik Persson <henrik.persson@iteam.com>

--- src/work-order/index.ts
+++ src/work-order/index.ts
@@ -16,11 +16,12 @@ interface WorkOrder {
   Status: string
   UseMasterKey: boolean
   WorkOrderRows: {
-    Description: string
-    LocationCode: string
-    EquipmentCode: string
+    Description: string | null
+    LocationCode: string | null
+    EquipmentCode: string | null
   }[]
   Messages?: WorkOrderMessage[]
+  Url?: string
 }
 
 interface WorkOrderMessage {
