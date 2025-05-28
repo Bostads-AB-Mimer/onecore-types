42f8150dc1a8f1282e11d0d3bc29c83cba153e96
Linda Almqvist <linda.almqvist@prototyp.se>
2025-05-28 16:22:54 +0200

    feat: UTH-250 add area types to RentalObject

--- src/types.ts
+++ src/types.ts
@@ -355,6 +355,9 @@ interface RentalObject {
   objectTypeCaption: string
   objectTypeCode: string
   vacantFrom: Date
+  braArea?: number
+  btaArea?: number
+  boaArea?: number
 }
 
 interface VacantParkingSpace extends RentalObject {
